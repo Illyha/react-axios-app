0e452d6c943725e4587f2a85bed6d8151a6e4e3c
Ілля Слівчук <illyasliva433456@ukr.net>
2019-04-08 20:54:22 +0300

    changes addeD

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
+const apiURL='https://jsonplaceholder.typicode.com/users/';
+
 class App extends Component {
+  constructor(){
+    super();
+    this.state= {
+      persons : [],
+      name : '',
+      id : '',
+      createdPersons : [],
+    }
+  }
+
+  componentDidMount(){
+    axios.get(apiURL)
+      .then(res => {
+        const persons = res.data;
+        this.setState({persons});
+      })
+      .catch( err=> {
+        console.log(`Error = ${err}`);
+      });
+  }
+
+  onChange = event => {
+    this.setState( {name : event.target.value} )
+  }
+
+  onSubmit = event => {
+    event.preventDefault();
+    const user = {name:this.state.name};
+
+    axios.post( apiURL, { user } )
+    .then(res => {
+      console.log(res);
+      console.log(res.data);
+      let createdPerson = res.data;
+      this.setState({createdPersons:[...this.state.createdPersons,createdPerson]});
+    })
+    .catch( err=> {
+      console.log(`Error is = ${err}`);
+  });
+}
+
+onSubmitDelete = event => {
+   event.preventDefault();
+   axios.delete(apiURL+ '' + this.state.id)
+   .then(res => {
+     console.log(res);
+     console.log(res.data);
+   })
+   .catch( err => {
+     console.log(`Error is = ${err}`);
+   });
+}
+
+onChangeDelete = event => { 
+  this.setState({id : event.target.value});
+}
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="crud-get">
+      <ul>
+        {this.state.persons.map((person,index) => <li key = {index}>{person.name}</li>)}
+      </ul>
+      </div>
+      <div className="crud-post">
+      <form onSubmit = {this.onSubmit}>
+      <label>
+      Person name : 
+      <input type="text" onChange = {this.onChange} name = "name" />
+      </label>
+      <button type="submit">Add new person</button>
+      </form>
+      <ul>
+        {this.state.createdPersons.map((person,index) => <li key = {index}>{person.user.name}</li>)}
+      </ul>
+      </div>
+
+      <div className="crud-delete">
+      <form onSubmit = {this.onSubmitDelete}>
+      <label>
+      Person id : 
+      <input type="text" onChange = {this.onChangeDelete} name = "id" />
+      </label>
+      <button type="submit">delete person</button>
+      </form>
+
+      </div>
       </div>
     );
   }
